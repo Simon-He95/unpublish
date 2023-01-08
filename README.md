b3687e40715b0d23cf7a483b0218a561b476c69f
Simon He <13917107469@163.com>
2023-01-08 23:06:07 +0800

    chore: update

---
+++ README.md
@@ -0,0 +1,30 @@
+# @simon_he/unpublish
+
+快速取消发布包版本
+
+## Install
+
+```
+npm i -g @simon_he/unpublish
+```
+
+## Usage
+
+```
+// 默认取消发布当前package.json中的version
+unpublish
+
+// 指定取消发布某个版本
+unpublish 0.0.1
+
+// params --force 强制取消发布
+unpublish 0.0.1 --force
+```
+
+## License
+
+[MIT](./LICENSE) License © 2022 [Simon He](https://github.com/Simon-He95)
+
+<a href="https://github.com/Simon-He95/sponsor" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
+
+<span><div align="center">![sponsors](https://www.hejian.club/images/sponsors.jpg)</div></span>
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import process from 'process'
+import { getPkg, jsShell, log, spaceFormat } from 'lazy-js-utils'
+import colorize from '@simon_he/colorize'
+import { version as VERSION } from '../package.json'
+
+export async function setup() {
+  const argv: string[] = process.argv.slice(2)
+  const params = spaceFormat(argv.join(' ')).trim()
+  if (params === '-v' || params === '--version') {
+    log(
+      colorize({
+        text: `@simon_he/unpublish version: ${VERSION}`,
+        color: 'blue',
+      }),
+      {},
+    )
+    return
+  }
+  const { name, version } = await getPkg()
+  let publishedVersion = `${name}@`
+  if (params)
+    publishedVersion += params
+  else publishedVersion += version + params
+
+  jsShell(`npm unpublish ${publishedVersion} ${params}`)
+}
+
+setup()
